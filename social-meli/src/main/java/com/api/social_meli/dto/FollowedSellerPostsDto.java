fd9d8d372620710228e5795b434b97a810f465ac
NadinaJauch <nadinajauch@gmail.com>
2024-12-13 15:28:59 -0300

    endpoint para obtener posts de usuarios seguidos

---
+++ social-meli/src/main/java/com/api/social_meli/dto/FollowedSellerPostsDto.java
@@ -0,0 +1,10 @@
+package com.api.social_meli.dto;
+
+import com.api.social_meli.model.Post;
+
+import java.util.List;
+
+public class SellerPostsDto {
+    private int userId;
+    private List<PostDto> posts;
+}
