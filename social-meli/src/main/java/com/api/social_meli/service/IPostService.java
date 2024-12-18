64f4696058bcaf1f1b1abc7af2399fa766a30c93
lvingman <r_martin@tutanota.com>
2024-12-18 10:04:03 -0300

    fix: cambia output de US0010 a MessageDto, validacion de category, y agrega valores en postcategory.json

--- social-meli/src/main/java/com/api/social_meli/service/IPostService.java
+++ social-meli/src/main/java/com/api/social_meli/service/IPostService.java
@@ -2,13 +2,14 @@ package com.api.social_meli.service;
 
 import com.api.social_meli.dto.*;
 import com.api.social_meli.dto.PromoPostDto;
+import org.apache.logging.log4j.message.Message;
 
 import java.util.List;
 
 public interface IPostService {
     List<PostDto> getPostsByUserId(int userId);
     PromoPostCountDto getPromoProductCount(Integer userId);
-    String createPromoPost(PromoPostDto dto);
+    MessageDto createPromoPost(PromoPostDto dto);
     MessageDto createPost(PostDto dto);
     FollowedSellerPostsDto getFollowedSellersPosts(int userId, String order);
     List<GetByCategoryDto> getPostByCategoryId(int categoryId, double price_min, double price_max);
