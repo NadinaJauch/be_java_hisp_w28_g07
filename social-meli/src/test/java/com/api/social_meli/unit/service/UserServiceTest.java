e114f0eb0a0dd8acd79d7e9618b109e44704eb02
lgiussanialo <lisandro.giussanialo@mercadolibre.com>
2024-12-30 16:11:19 -0300

    Applied user creation via factory

--- social-meli/src/test/java/com/api/social_meli/unit/service/UserServiceTest.java
+++ social-meli/src/test/java/com/api/social_meli/unit/service/UserServiceTest.java
@@ -1,25 +1,20 @@
 package com.api.social_meli.unit.service;
-
 import com.api.social_meli.dto.GetFollowerCountDto;
 import com.api.social_meli.dto.MessageDto;
 import com.api.social_meli.exception.NotFoundException;
 import com.api.social_meli.model.User;
 import com.api.social_meli.repository.impl.UserRepositoryImpl;
 import com.api.social_meli.service.impl.UserServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
+import com.api.social_meli.util.MockFactoryUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -30,21 +25,16 @@ public class UserServiceTest {
     @InjectMocks
     private UserServiceImpl userService;
 
-    //region T-0002 - US-0007 UNFOLLOW USER
+    private final MockFactoryUtils userFactoryUtils = new MockFactoryUtils();
+
+    //region UNFOLLOW USER
     @Test
     void unfollowValidUsersSuccessfullyUnfollows() {
         // ARRANGE
         int userId = 1;
         int userIdToUnfollow = 2;
-
-        User user = new User();
-        user.setId(userId);
-        user.setFollowed(new ArrayList<>(List.of(userIdToUnfollow)));
-
-        User userToUnfollow = new User();
-        userToUnfollow.setId(userIdToUnfollow);
-        userToUnfollow.setFollowers(new ArrayList<>(List.of(userId)));
-
+        User user = userFactoryUtils.createUserWithOnlyFollowersAndFolloweds(userId, new ArrayList<>(List.of(userIdToUnfollow)), new ArrayList<>());
+        User userToUnfollow = userFactoryUtils.createUserWithOnlyFollowersAndFolloweds(userIdToUnfollow, new ArrayList<>(),  new ArrayList<>(List.of(userId)));
         when(userRepository.findById(userId)).thenReturn(user);
         when(userRepository.findById(userIdToUnfollow)).thenReturn(userToUnfollow);
 
@@ -59,33 +49,27 @@ public class UserServiceTest {
 
     @Test
     void unfollowNotExistentUserThrowsNotFoundEx() {
-
+        //ARRANGE
         int userId = 1;
         int userIdToUnfollow = 23232;
-
         User user = new User();
         user.setId(userId);
-
         when(userRepository.findById(userId)).thenReturn(user);
         when(userRepository.findById(userIdToUnfollow)).thenReturn(null);
 
+        //ACT & ASSERT
         assertThrows(NotFoundException.class, () -> userService.unfollowUser(userId, userIdToUnfollow));
-
     }
-
     //endregion
 
-
-    //region T-0007 US-0002 VERIFY FOLLOWER COUNT
+    //region VERIFY FOLLOWER COUNT
 
     @Test
     void getFollowerCountValidUserReturnsCorrectCount(){
-
         //ARRANGE
         User user = new User();
         user.setId(1);
         user.setFollowers(new ArrayList<>(List.of(2,3,4)));
-
         when(userRepository.findById(1)).thenReturn(user);
 
         //ACT
@@ -94,9 +78,5 @@ public class UserServiceTest {
         //ASSERT
         assertEquals(3,getFollowerCountDto.getFollowers_count());
     }
-
-//endregion
-
-
-
+    //endregion
 }
