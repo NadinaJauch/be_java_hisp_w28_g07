f01ad5821404dd27988409ecb7f65fbdde1e91a4
lgiussanialo <lisandro.giussanialo@mercadolibre.com>
2024-12-30 16:11:19 -0300

    Applied user creation via factory

--- social-meli/src/test/java/com/api/social_meli/util/MockFactoryUtils.java
+++ social-meli/src/test/java/com/api/social_meli/util/MockFactoryUtils.java
@@ -11,7 +11,6 @@ import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.time.LocalDate;
 import java.util.List;
