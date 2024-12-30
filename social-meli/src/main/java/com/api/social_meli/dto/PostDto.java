ec591e22307cb14d23a1546bed541b4bffdaed12
lvingman <r_martin@tutanota.com>
2024-12-30 17:12:07 -0300

    fix: removes validation for postId

--- social-meli/src/main/java/com/api/social_meli/dto/PostDto.java
+++ social-meli/src/main/java/com/api/social_meli/dto/PostDto.java
@@ -16,8 +16,6 @@ import java.time.format.DateTimeFormatter;
 @NoArgsConstructor
 @Data
 public class PostDto {
-    @Min(value = -1, message = "El id debe ser mayor a cero")
-    //TODO: Ints por ser primitivos no pueden ser nulls, siempre seran ceros al no tener un valor declarado. Mencionar al team de que existe la chance de que haya que refactorizar todos los ints de DTOs a Integers.
     @JsonProperty("post_id")
     private int postId;
     @Min(value = -1, message = "El id debe ser mayor a cero")
